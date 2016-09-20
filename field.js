dfb910b230aeb1ee40295fa0885a5ba86d0f6374
daiki-wekids <daiki@wekids-inc.com>
2016-09-20 12:34:16 +0900

    Almost completed 2048

--- field.js
+++ field.js
@@ -1,35 +1,62 @@
-(function　field() {
+
 	const row = 4;	//マスの数 (定数)
 	const col = 4;
 
-	var randTile1 = Math.floor( Math.random() * 2 ) + 1 ;	// 1or2 をrandTile1に入れる
-	var randTile2 = Math.floor( Math.random() * 2 ) + 1 ;
+	grid = [];
 
-	var randRow1 = Math.floor( Math.random() * row ) + 1 ;	// 1 ~ 4 をrandRow1に入れる
-	var randCol1 = Math.floor( Math.random() * col ) + 1 ;
-	var randRow2 = Math.floor( Math.random() * row ) + 1 ;
-	var randCol2 = Math.floor( Math.random() * col ) + 1 ;
-
-	if(randRow1===randRow2 && randCol1===randCol2) {	// ランダムマスが被ったら書き換える
-		randRow2 = (row + 1) - randRow2;
-		randCol2 = (col + 1) - randCol2;
+	for(var i = 1; i <= col; i++){	// col * row のgrid配列すべてに0を入れる
+		grid[i] = [];
+		for(var j = 1; j <= row; j++){
+			grid[i][j] = 0;
+		}
 	}
 
-	board = [];		//ボード配列
+	function newTile() {
 
-	for(var i = 1; i <= col; i++){	// 4*4のボード
-		board[i] = [];
-		for(var j = 1; j <= row; j++){
-			board[i][j] = 0;
+		twoOrFour = Math.floor( Math.random() * 2 ) + 1 ;
+
+		for (var i = 0; i < 1000; i++) {
+
+			randRow = Math.floor( Math.random() * row ) + 1 ;
+			randCol = Math.floor( Math.random() * col ) + 1 ;
+
+			if (grid[randRow][randCol] === 0) {
+				grid[randRow][randCol] = twoOrFour;
+				break;
+			}
 		}
+		showBoard();
 	}
 
 	function　showBoard() {
 		var b = document.getElementById("board");
+
+		tile = [document.getElementById("cell"),	//配列　添字　0
+				document.getElementById("num2"),		// 1
+				document.getElementById("num4"),		// 2
+				document.getElementById("num8"),		// 3
+				document.getElementById("num16"),		// 4
+				document.getElementById("num32"),		// 5
+				document.getElementById("num64"),		// 6
+				document.getElementById("num128"),		// 7
+				document.getElementById("num256"),		// 8
+				document.getElementById("num512"),		// 9
+				document.getElementById("num1024"),	// 10
+				document.getElementById("num2048"),]	// 11
+
+		for(var i = 1; i <= col; i++){	// ボードすべてに0を入れる
+			//grid[i] = [];
+			for(var j = 1; j <= row; j++){
+				//grid[i][j] = 0;
+				if (grid[i][j] >= 100) {
+					grid[i][j] = grid[i][j] - 100;
+				}
+			}
+		}
 					
 		for(var y = 1; y <= col; y++) {		// 68pxずつずらして表示
 			for(var x = 1; x <= row; x++) {
-				var c = piece[board[x][y]].cloneNode(true);
+				var c = tile[grid[x][y]].cloneNode(true);
 				c.style.left = ((x - 1) * 68) + "px";
 				c.style.top = ((y - 1) * 68) + "px";
 				b.appendChild(c);
@@ -47,39 +74,32 @@
 		}
 	}
 				
-	onload = function() {
+	function initGame() {	//ゲームを初期状態にする
 
-		piece = [document.getElementById("cell"),	//配列　添字　0
-				document.getElementById("2"),		// 1
-				document.getElementById("4"),		// 2
-				document.getElementById("8"),		// 3
-				document.getElementById("16"),		// 4
-				document.getElementById("32"),		// 5
-				document.getElementById("64"),		// 6
-				document.getElementById("128"),		// 7
-				document.getElementById("256"),		// 8
-				document.getElementById("512"),		// 9
-				document.getElementById("1024"),	// 10
-				document.getElementById("2048"),]	// 11
+		for(var i = 1; i <= col; i++){	// ボードすべてに0を入れる
+			grid[i] = [];
+			for(var j = 1; j <= row; j++){
+				grid[i][j] = 0;
+			}
+		}
 
-		board[randRow1][randCol1] = randTile1;		//ランダムなrow colにrandTile(1or2)を入れる
-		board[randRow2][randCol2] = randTile2;
+		newTile();
+		newTile();
 
-		/*	全タイル表示テスト
-		board[1][1] = 1;
-		board[1][2] = 2;
-		board[1][3] = 3;
-		board[1][4] = 4;
-		board[2][1] = 5;
-		board[2][2] = 6;
-		board[2][3] = 7;
-		board[2][4] = 8;
-		board[3][1] = 9;
-		board[3][2] = 10;
-		board[3][3] = 11;
+		/* 全タイル表示テスト
+		grid[1][1] = 1;
+		grid[1][2] = 2;
+		grid[1][3] = 3;
+		grid[1][4] = 4;
+		grid[2][1] = 5;
+		grid[2][2] = 6;
+		grid[2][3] = 7;
+		grid[2][4] = 8;
+		grid[3][1] = 9;
+		grid[3][2] = 10;
+		grid[3][3] = 11;
 		*/
 
 		showBoard();
-	};
+	}
 
-})();
